a11e98496c1b06d8560dada1843cdc1ef65dedd1
Bryan Thompson <conundrum2k@gmail.com>
2014-03-03 18:36:53 -0800

    switched to 12 hour format - started cal
    started calendar section, switched time to 12 hour format for the time being

--- src/testWatchface.c
+++ src/testWatchface.c
@@ -11,6 +11,9 @@
 // App-specific data
 Window *window; // All apps must have at least one window
 TextLayer *time_layer; // The clock
+TextLayer *date_layer;
+TextLayer *day1_layer;
+
 static GBitmap *background_image;  // declare background image
 static BitmapLayer *background_layer; // background image layer
 
@@ -20,9 +23,8 @@ static void handle_minute_tick(struct tm* tick_time, TimeUnits units_changed) {
   static char time_text[] = "00:00"; // Needs to be static because it's used by the system later.
 
 
-  strftime(time_text, sizeof(time_text), "%T", tick_time);
+  strftime(time_text, sizeof(time_text), "%l:%M", tick_time);
   text_layer_set_text(time_layer, time_text);
-  
 }
 
 
@@ -44,11 +46,32 @@ static void do_init(void) {
 
   
   // Init the text layer used to show the time
-  time_layer = text_layer_create(GRect(5, 9, 144-5, 56-9 /* height */));
+  time_layer = text_layer_create(GRect(5, 9, 142-5, 56-9 /* height */));
   text_layer_set_text_color(time_layer, GColorBlack);
   text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
   text_layer_set_background_color(time_layer, GColorClear);
   text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD));
+
+  date_layer = text_layer_create(GRect(5, 112, 142-5, 128-112));
+  text_layer_set_text_color(date_layer, GColorBlack);
+  text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(date_layer, GColorClear);
+  text_layer_set_font(date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+  layer_add_child(window_layer, text_layer_get_layer(date_layer));
+  
+  day1_layer = text_layer_create(GRect(10, 125, 22-10, 174-130));
+  text_layer_set_text_color(day1_layer, GColorBlack);
+  text_layer_set_text_alignment(day1_layer, GTextAlignmentLeft);
+  text_layer_set_background_color(day1_layer, GColorClear);
+  text_layer_set_font(day1_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  layer_add_child(window_layer, text_layer_get_layer(day1_layer));
+  
+  
+  text_layer_set_text(date_layer, "Su Mo Tu We Th Fr Sa");
+  text_layer_set_text(day1_layer, "01");
+
+  
+  
   
   // Ensures time is displayed immediately (will break if NULL tick event accessed).
   // (This is why it's a good idea to have a separate routine to do the update itself.)
@@ -66,9 +89,12 @@ static void do_deinit(void) {
   bitmap_layer_destroy(background_layer);
   gbitmap_destroy(background_image);
   
-  
   text_layer_destroy(time_layer);
+  text_layer_destroy(date_layer);
+  text_layer_destroy(day1_layer);
   window_destroy(window);
+  
+  
   
   
 }
